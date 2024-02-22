5f44ddf7e2888d1e853f3bbf2f56dcb3a9c68c65
molammed <alshmoha@shernet.sheridancollege.ca>
2024-02-21 23:05:20 -0500

    jhnjunjdnjqnvjcjn brwjf

---
+++ midtermm/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace midtermm;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ midtermm/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace midtermm;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
